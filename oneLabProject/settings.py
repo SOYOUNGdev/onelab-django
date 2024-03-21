88fed4a27af2da9c638d39b569226aa634e86911
SOYOUNGdev <soyoungim.sy@gmail.com>
2024-03-21 09:02:44 +0900

    settings에 원랩추가

--- oneLabProject/settings.py
+++ oneLabProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m&1gjc)a!0m&t4-skkigdplzw%e&f)5j@ksztwce%#bqaksqs)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '13.125.239.155']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '13.125.239.155', 'onelab.today']
 
 # Application definition
 
